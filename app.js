55c203e8d251ed162f5acc705cb98e137c7d4d80
juanmayorquin <juanjmayor@outlook.es>
2024-12-19 09:47:53 -0500

    Agregar soporte para MongoDB

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const cors = require("cors")
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
+const URI = process.env.URI;
 
 let notes = [
   {
